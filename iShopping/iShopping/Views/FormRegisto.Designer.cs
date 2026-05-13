05daf506f4e795dd058029a84e578cb2fdd68b2e
Thomaz Cortez Faria Barrago <2025193333@my.ipleiria.pt>
2026-05-13 16:16:25 +0100

    Design Simples FormLogin & FormRegisto
    (+ LinkLabels para conseguir ir de uma para outra)

---
+++ iShopping/iShopping/Views/FormRegisto.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace iShopping.Views
+{
+    partial class FormRegisto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkRedirect = new System.Windows.Forms.LinkLabel();
+            this.btnRegistar = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbRepetirPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // linkRedirect
+            // 
+            this.linkRedirect.AutoSize = true;
+            this.linkRedirect.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkRedirect.Location = new System.Drawing.Point(244, 395);
+            this.linkRedirect.Name = "linkRedirect";
+            this.linkRedirect.Size = new System.Drawing.Size(293, 30);
+            this.linkRedirect.TabIndex = 13;
+            this.linkRedirect.TabStop = true;
+            this.linkRedirect.Text = "Já tem uma conta? Entre aqui.";
+            this.linkRedirect.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkRedirect_LinkClicked);
+            // 
+            // btnRegistar
+            // 
+            this.btnRegistar.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistar.Location = new System.Drawing.Point(335, 303);
+            this.btnRegistar.Name = "btnRegistar";
+            this.btnRegistar.Size = new System.Drawing.Size(141, 61);
+            this.btnRegistar.TabIndex = 12;
+            this.btnRegistar.Text = "Registar";
+            this.btnRegistar.UseVisualStyleBackColor = true;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(173, 183);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(595, 31);
+            this.tbPassword.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 37);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Password:";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(173, 109);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(595, 31);
+            this.tbUsername.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 59);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "iShopping";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbRepetirPassword
+            // 
+            this.tbRepetirPassword.Location = new System.Drawing.Point(256, 251);
+            this.tbRepetirPassword.Name = "tbRepetirPassword";
+            this.tbRepetirPassword.PasswordChar = '*';
+            this.tbRepetirPassword.Size = new System.Drawing.Size(512, 31);
+            this.tbRepetirPassword.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 37);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Repetir Password:";
+            // 
+            // FormRegisto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbRepetirPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkRedirect);
+            this.Controls.Add(this.btnRegistar);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormRegisto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormRegisto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkRedirect;
+        private System.Windows.Forms.Button btnRegistar;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbRepetirPassword;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ iShopping/iShopping/Views/FormRegisto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iShopping.Views
+{
+    public partial class FormRegisto : Form
+    {
+        public FormRegisto()
+        {
+            InitializeComponent();
+        }
+
+        private void linkRedirect_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FormLogin formLogin = new FormLogin();
+            formLogin.Show();
+            this.Close();
+        }
+    }
+}
